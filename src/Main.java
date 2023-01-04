fc9b7fa7b2116c26096bd6d49f1b101ab87558b2
ehsan.ab101 <ehsan.ab101@gmail.com>
2023-01-04 06:10:52 -0800

    main config done

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
 import  ast.nodes.*;
 import  ast.variables.*;
+
 import gen.dart_parse;
 import gen.dart_lexar;
+
 import java.io.IOException;
 import org.antlr.v4.runtime.CharStream;
 import org. antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
-import static org.antlr.v4. runtime. CharStreams. fromFileName;
+import static org.antlr.v4.runtime.CharStreams.fromFileName;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String source = "myLanguage. txt";
+        String source = "testFile.txt";
         CharStream cs = fromFileName (source);
-        dart_lexar lexer = new dart_lexar (cs);
+        dart_lexar lexer = new dart_lexar(cs);
         CommonTokenStream token = new CommonTokenStream(lexer);
-        MyLanquageParser parser = new MyLanquageParser (token);
-        ParseTree tree = parser -page (
-                Paqe doc = (Paqe) new BaseVisitor().visit(tree);
+
+        dart_parse parser = new dart_parse(token);
+        ParseTree tree = parser.topTreeDeclaration();
+        TopTreeDeclaration doc = (TopTreeDeclaration) new NodesVisitor().visit(tree);
     }
 
 }
