f5a48364fc51cb88f6ddb38d6abb2d233eaa6c57
ritarezek200185 <ritarezek200185@gmail.com>
2023-01-06 15:36:08 +0300

    visitMultiplyExpression (done)

--- src/ast/variables/AddExpression.java
+++ src/ast/variables/AddExpression.java
@@ -1,28 +1,16 @@
 package ast.variables;
 
 public class AddExpression {
-	MultiplyExpression left;
-	MultiplyExpression right;
-	String operation;
-	
-	public AddExpression(MultiplyExpression left){
-		this.left=left;
+	NumberClass value;
+
+
+	public AddExpression(NumberClass value) {
+		this.value = value;
+
 	}
-	
-	public AddExpression(MultiplyExpression left,MultiplyExpression right,String operation){
-		this.left=left;
-		this.right=right;
-		this.operation=operation;
-	}
-	
+
 	@Override
 	public String toString() {
-		if(operation!="") {
-			return  left.toString();
-		}else {
-			return  left.toString() + operation + right.toString();
-		}
-		
+		return value.toString();
 	}
-	
 }
--- src/ast/variables/MultiplyExpression.java
+++ src/ast/variables/MultiplyExpression.java
@@ -3,6 +3,13 @@ package ast.variables;
 public class MultiplyExpression {
     NumberClass value;
 
+    public NumberClass getValue() {
+        return value;
+    }
+
+    public void setValue(NumberClass value) {
+        this.value = value;
+    }
 
     public MultiplyExpression(NumberClass value) {
         this.value = value;
