97fd7d1230e7d6ad169eef3739afc161c27d2307
ritarezek200185 <ritarezek200185@gmail.com>
2023-01-06 19:50:48 +0300

    variablesVisitor (done)

--- src/Main.java
+++ src/Main.java
@@ -21,8 +21,8 @@ public class Main{
         CommonTokenStream token = new CommonTokenStream(lexer);
         dart_parse parser = new dart_parse(token);
 
-        ParseTree tree = parser.widgetsDeclaration();
-        WidgetsDeclaration doc = (WidgetsDeclaration) new NodesVisitor().visit(tree);
+        ParseTree tree = parser.variable();
+        Variable doc = (Variable) new VariablesVisitor().visit(tree);
         System.out.println(doc);
     }
 
--- src/ast/variables/DoubleDeclaration.java
+++ src/ast/variables/DoubleDeclaration.java
@@ -1,20 +1,23 @@
 package ast.variables;
 
 public class DoubleDeclaration extends VariableDeclaration {
-	String name;
-	AddDoubleExpression expr;
-	
-	public DoubleDeclaration(String name) {
-		this.name=name;
-	}
-	
-	public DoubleDeclaration(AddDoubleExpression expr,String name) {
-		this.expr = expr;
-		this.name = name;
-	}
-	
-	@Override
-	public String toString() {
-		return  name;
-	}
+    String name;
+    AddDoubleExpression expr;
+
+    public DoubleDeclaration(String name) {
+        this.name = name;
+    }
+
+    public DoubleDeclaration(AddDoubleExpression expr, String name) {
+        this.expr = expr;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        if (expr != null) {
+            return "\n" + name + " = " + expr;
+        }
+        return "\n" + name;
+    }
 }
