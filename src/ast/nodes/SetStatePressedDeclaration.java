6d0cf7600c063b9c17b5495887b154655175c009
ehsan.ab101 <ehsan.ab101@gmail.com>
2023-01-07 07:27:02 -0800

    added statement visitor and on pressed and set state

---
+++ src/ast/nodes/SetStatePressedDeclaration.java
@@ -0,0 +1,19 @@
+package ast.nodes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SetStatePressedDeclaration {
+
+
+    List<StatementDeclaration> statementDeclaration;
+
+    public List<StatementDeclaration> getStatementDeclaration() {
+        return statementDeclaration;
+    }
+
+    public SetStatePressedDeclaration(){
+        statementDeclaration = new ArrayList<>();
+    };
+
+}
