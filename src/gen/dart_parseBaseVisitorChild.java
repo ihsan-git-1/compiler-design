db3b530ea997042da7827cb0183f843ae5c250ed
GeoN <geon200181@gmail.com>
2023-04-15 18:55:38 +0300

    Missing file dartParseBaseVisitor

--- src/gen/dart_parseBaseVisitorChild.java
+++ src/gen/dart_parseBaseVisitorChild.java
@@ -1,16 +1,33 @@
 package gen;
 
+import ast.Scope;
 import ast.SymbolTableObject;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class dart_parseBaseVisitorChild extends dart_parseBaseVisitor{
 
     public static List<String> semanticErrors = new ArrayList<>();
-//	public static List<String> vars= new ArrayList<>();
 
-    public static Map<String, SymbolTableObject> symbolTable = new HashMap<>();
+    public static ArrayList<Scope> symbolTable = new ArrayList<Scope>();
+
+    public static ArrayList<Scope> scopes=new ArrayList<>();
+    public static int index=1;
+
+
+    public boolean CheckExistanceInScope(String id , int index ){
+        for(Scope s : scopes){
+
+            if(s.getId() == index){
+                for (Map.Entry<String, SymbolTableObject> mapElement : s.getSymbolMap().entrySet()) {
+
+
+                    if(mapElement.getKey()==id){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
