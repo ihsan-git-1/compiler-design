e6149b5f621773ed43264cd3e515911b44049630
ihsan-git-1 <ihsan.abourshaid.git1@gmail.com>
2023-05-06 03:22:01 +0400

    function refactor done

--- src/ast/nodes/ReturnStatement.java
+++ src/ast/nodes/ReturnStatement.java
@@ -1,20 +1,22 @@
 package ast.nodes;
 
+import gen.dart_parse;
+
 import java.beans.Expression;
 
 public class ReturnStatement extends Node{
     Expression expression;
 
-    public ReturnStatement(int line, String parent, String type, int childCount, Expression expression) {
-        super(line, parent, type, childCount);
+    public ReturnStatement(dart_parse.ReturnStatementContext ctx, Expression expression) {
+        super(ctx);
         this.expression = expression;
     }
 
     @Override
     public String toString() {
-        return "ReturnStatement{" +
+        return getLineString()
+                +"ReturnStatement{" +
                 "expression=" + expression +
-                ", line=" + line +
                 ", col=" + col +
                 ", type='" + type + '\'' +
                 ", childCount=" + childCount +
