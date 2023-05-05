dd191ebc499edc12189b623568b001b86800e0a9
ehsanabourshaed <ehsan.ab101@gmail.com>
2023-05-05 22:40:04 +0400

    navigation visitor added and done

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ import static org.antlr.v4.runtime.CharStreams.fromFileName;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String source = "src/tests/evaluations/test1.txt";
+        String source = "src/tests/declarations/navigation_test/test.txt";
         CharStream cs = fromFileName(source);
         dart_lexar lexer = new dart_lexar(cs);
         CommonTokenStream token = new CommonTokenStream(lexer);
