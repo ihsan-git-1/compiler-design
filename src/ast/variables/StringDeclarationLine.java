a973f3c85053967a7dade2a8053e4b0ac45d43b6
ehsanabourshaed <ehsan.ab101@gmail.com>
2023-05-06 06:24:50 +0400

    string visitor done

--- src/ast/variables/StringDeclarationLine.java
+++ src/ast/variables/StringDeclarationLine.java
@@ -1,13 +1,16 @@
 package ast.variables;
 
+import ast.NodeType;
+import gen.dart_parse;
+
 import java.util.List;
 
 public class StringDeclarationLine extends VariableDeclaration {
     List<StringDeclaration> declarations;
     List<StringDeclarationAssignment> assignments;
 
-    public StringDeclarationLine(int line, String parent,String type,int childCount, List<StringDeclaration> declarations, List<StringDeclarationAssignment> assignments) {
-        super(line, parent, type, childCount);
+    public StringDeclarationLine(dart_parse.StringDeclarationLineContext ctx, List<StringDeclaration> declarations, List<StringDeclarationAssignment> assignments) {
+        super(ctx);
         this.declarations = declarations;
         this.assignments = assignments;
     }
@@ -20,11 +23,10 @@ public class StringDeclarationLine extends VariableDeclaration {
     }
 
     public String toString() {
-        return "String Declaration Line: "
+        return getLineString()+"String Declaration Line: "
                 +"Number of declarations on this line: " + this.declarations.size()
                 +"Number of assignments on this line: " + this.assignments.size()
-                +" line: "+ getLine()
                 + " parent: "+ getParent()
-                +" Child Count =  "+getChildCount()+" Type = "+getType()+"\n";
+                +" Child Count =  "+getChildCount()+" Type = "+ NodeType.STRINGDECLARATIONLINE+"\n";
     }
 }
