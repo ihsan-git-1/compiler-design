8bf6ce7c6b2d22828af3d28d6a6183aebc0a512e
ihsan-git-1 <ihsan.abourshaid.git1@gmail.com>
2023-05-19 00:31:58 +0400

    navigation almost done

--- src/visitors/DartVisitors/NavigationVisitor.java
+++ src/visitors/DartVisitors/NavigationVisitor.java
@@ -5,18 +5,45 @@ import ast.navigation.Navigation;
 import gen.dart_parse;
 import visitors.dart_parseBaseVisitorChild;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class NavigationVisitor extends dart_parseBaseVisitorChild {
 
 
     @Override
     public Navigation visitNavigation(dart_parse.NavigationContext ctx) {
-        int line = ctx.start.getLine();
-        String type = NodeType.Navigation.toString();
-        String parent = ctx.getParent().getClass().getName().replace("gen.dart_parse$", "").replace("Context", "");
 
-        int childCount = ctx.getChildCount();
+        Navigation navigation = new Navigation(ctx,ctx.STRING_LINE().getText());
 
-        return new Navigation(ctx,ctx.STRING_LINE().getText());
+        System.out.println(navigation.getName());
+
+        String htmlFilePath = "src/tests/declarations/navigation_test/"+ "test" +".html";
+
+        File htmlFile = new File(htmlFilePath);
+
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            if (htmlFile.exists()) {
+                try {
+                    String name = "John";
+                    String url = htmlFile.toURI().toString() + "?name=asdas";
+                    System.out.println("URL: " + url);
+
+                    // Open the URL in the default browser
+                    desktop.browse(new URI(url));
+                } catch (IOException e ) {
+                    System.out.println("Can't launch html files");
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return navigation;
 
     }
 }
